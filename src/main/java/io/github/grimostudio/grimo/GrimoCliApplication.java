5e6f4754abfa00526a340c2a5146194e30957ccf
samzhu <spike19820318@gmail.com>
2026-02-03 23:40:18 +0800

    first commit

---
+++ src/main/java/io/github/grimostudio/grimo/GrimoCliApplication.java
@@ -0,0 +1,13 @@
+package io.github.grimostudio.grimo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrimoCliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrimoCliApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/grimostudio/grimo/GrimoCliApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.grimostudio.grimo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrimoCliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
